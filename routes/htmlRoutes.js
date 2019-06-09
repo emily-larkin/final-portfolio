127e04685a857af5d7082fb3f43f75369d41082a
emssslay <emily.robinson@iechs.org>
2019-06-09 11:17:55 -0600

    cleaning up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,14 +15,6 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-//   app.get("/tables", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/tables.html"));
-//   });
-
-//   app.get("/reserve", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/reserve.html"));
-//   });
-
   // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/home.html"));
